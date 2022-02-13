f31f671d3f8680fd66a28b140998a79ae7dcdd67
Александр Иконников <ikonnikovAI@lad24.ru>
2022-02-13 14:14:14 +0300

    add validation to user dto

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,17 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginUserDto {
   @ApiProperty()
+  @IsEmail()
   email: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
 
 export class CreateUserDto extends LoginUserDto {
   @ApiProperty()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   surname: string;
 
   @ApiProperty()
